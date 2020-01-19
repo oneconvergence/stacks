85e1719108170d175fa8ab91151f441c60d57982
Rajendra Singh <rajendra.singh@oneconvergence.com>
2020-01-19 11:03:16 -0600

    move dkube job related code in sperate file

--- stable/dkube-datascience/templates/tensorflow/app.py
+++ stable/dkube-datascience/templates/tensorflow/app.py
@@ -1,31 +1,15 @@
 from __future__ import print_function
+import os
 import ptvsd
 import argparse
 import tensorflow as tf
-import os, sys
-import signal
-import configparser, yaml, time
-
-def sigterm_handler(_signo, _stack_frame):
-    print("sigterm_handler executed, %s, %s" % (_signo, _stack_frame))
-    sys.exit(0)
-
-def update_job_name():
-    #generage uniq job name
-    y = yaml.safe_load(open(".appsody-config.yaml"))
-    config = configparser.ConfigParser()
-    config.read('.dkube_job.ini')
-    config['CUSTOM_JOB']["name"] = '"{}"'.format(y["project-name"] + "-" + time.strftime("%Y%m%d-%H%M%S"))
-    with open(".dkube_job.ini", "w") as f:
-        config.write(f)
+from dkube_job import dkube_run
 
 if __name__ == "__main__":
     run_in_dkube = os.environ.get("RUN_IN_DKUBE", "false")
 
     if run_in_dkube == "true":
-        signal.signal(signal.SIGTERM, sigterm_handler)
-        update_job_name()
-        exit_code = os.system ("/project/dkubectl customjob start -c .dkube_job.ini")
+        exit_code = dkube_run()
         sys.exit (exit_code)
         
     parser = argparse.ArgumentParser()
---
+++ stable/dkube-datascience/templates/tensorflow/dkube_job.py
@@ -0,0 +1,21 @@
+import os, sys
+import signal
+import configparser, yaml, time
+
+def sigterm_handler(_signo, _stack_frame):
+    print("sigterm_handler executed, %s, %s" % (_signo, _stack_frame))
+    sys.exit(0)
+
+def update_job_name():
+    #generage uniq job name
+    y = yaml.safe_load(open(".appsody-config.yaml"))
+    config = configparser.ConfigParser()
+    config.read('.dkube_job.ini')
+    config['CUSTOM_JOB']["name"] = '"{}"'.format(y["project-name"] + "-" + time.strftime("%Y%m%d-%H%M%S"))
+    with open("/tmp/.dkube_job.ini", "w") as f:
+        config.write(f)
+
+def dkube_run():
+    signal.signal(signal.SIGTERM, sigterm_handler)
+    update_job_name()
+    return os.system ("/project/dkubectl customjob start -c /tmp/.dkube_job.ini")
