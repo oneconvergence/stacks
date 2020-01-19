d5cc482c02456b3a20fbf026d3441b349c4cd361
Rajendra Singh <rajendra.singh@oneconvergence.com>
2020-01-18 21:54:04 -0600

    updated tensorflow template

--- stable/dkube-datascience/templates/tensorflow/app.py
+++ stable/dkube-datascience/templates/tensorflow/app.py
@@ -3,12 +3,29 @@ import ptvsd
 import argparse
 import tensorflow as tf
 import os, sys
+import signal
+import configparser, yaml, time
+
+def sigterm_handler(_signo, _stack_frame):
+    print("sigterm_handler executed, %s, %s" % (_signo, _stack_frame))
+    sys.exit(0)
+
+def update_job_name():
+    #generage uniq job name
+    y = yaml.safe_load(open(".appsody-config.yaml"))
+    config = configparser.ConfigParser()
+    config.read('.dkube_job.ini')
+    config['CUSTOM_JOB']["name"] = '"{}"'.format(y["project-name"] + "-" + time.strftime("%Y%m%d-%H%M%S"))
+    with open(".dkube_job.ini", "w") as f:
+        config.write(f)
 
 if __name__ == "__main__":
-    run_in_dkube = os.environ("RUN_IN_DKUBE", "false")
+    run_in_dkube = os.environ.get("RUN_IN_DKUBE", "false")
 
     if run_in_dkube == "true":
-        exit_code = os.system ("dkubectl customjob start -c .dkube_job.ini")
+        signal.signal(signal.SIGTERM, sigterm_handler)
+        update_job_name()
+        exit_code = os.system ("/project/dkubectl customjob start -c .dkube_job.ini")
         sys.exit (exit_code)
         
     parser = argparse.ArgumentParser()
